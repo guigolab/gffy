08db341625f1958f8629a574c3c560d709c55c52
apollo994 <fabio.zanarello.94@gmail.com>
2025-11-05 17:42:23 +0100

    Initialize gffy package

--- gffy/__init__.py
+++ gffy/__init__.py
@@ -4,10 +4,9 @@ gffy - GFF3 Genomic Statistics Calculator
 A fast and efficient tool for computing comprehensive statistics from GFF3 genomic annotation files.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 __author__ = "Emilio R"
 
 from .compute_stats import compute_gff_stats
 
 __all__ = ["compute_gff_stats"]
-
